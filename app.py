c0bab607bc774bfdebc2646842aaf72f9817de91
workspaceuser <workspaceuser@plot.ly>
2025-04-28 20:16:23 +0000

    Roland helps me change menu

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import textwrap
-from dash import Dash, dcc, page_container, callback, Output, Input
+from dash import Dash, dcc, callback, Output, Input, State
 import dash_design_kit as ddk
 import plotly.express as px
-from sqlalchemy.schema import DropColumnComment
 from theme import theme
 import xarray as xr
 import pandas as pd
@@ -11,11 +9,14 @@ import redis
 import os
 import json
 from datetime import datetime, timezone
+import plotly.graph_objects as go
+import dash_bootstrap_components as dbc
+
 
 redis_instance = redis.StrictRedis.from_url(os.environ.get("REDIS_URL", "redis://127.0.0.1:6379"))
 
 #app = Dash(__name__, use_pages=True)
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 server = "https://data.pmel.noaa.gov/pmel/erddap"
 e = ERDDAP(server=server, protocol="tabledap", response="nc")
@@ -37,6 +38,7 @@ for dataset_ID in pd.read_csv(end_url)["Dataset ID"]:
     # project = ds.attrs['project']
     if project not in projects:
         projects.append(project)
+
 
 app.layout = ddk.App(show_editor=False, theme=theme, children=[
     dcc.Store(id='selected_range'),
@@ -61,7 +63,9 @@ app.layout = ddk.App(show_editor=False, theme=theme, children=[
                     dcc.Loading(dcc.Dropdown(id="variables", multi=False, placeholder="Select Variable"))
                 ]),
             ]),
-            ddk.Card(width=0.7, children=[dcc.Loading(dcc.Graph(id='timeseries'))])
+            ddk.Card(width=0.7, children=[dcc.Loading(dcc.Graph(id='graph'))])
+            #ddk.Card(width=0.35, children=[dcc.Loading(dcc.Graph(id='1D_timeseries'))]),
+            #ddk.Card(width=0.35, children=[dcc.Loading(dcc.Graph(id='2D_timeseries'))])
         ]),
     ]),
     # ddk.Row(children=[
@@ -70,9 +74,19 @@ app.layout = ddk.App(show_editor=False, theme=theme, children=[
     # ]),
     #     ddk.Card(width=0.7, children=[dcc.Loading(dcc.Graph(id='trajectory'))]),
     # ]),
+
     ddk.Row(children=[
         ddk.Card(width=1, children=[dcc.Loading(dcc.Graph(id='trajectory'))]),
     ]),
+
+    # ddk.Row(children=[
+    #     dbc.Accordion([
+    #     dbc.AccordionItem([
+    #     ddk.Card(width=1, children=[dcc.Loading(dcc.Graph(id='trajectory'))]),
+    # ], title="Trajectory")
+    # ])
+    # ]),
+
     ddk.Row(children=[
         ddk.Card(width=1, id="project_information_card", children=[
         dcc.Loading(dcc.Markdown(id="project_information"))
@@ -80,12 +94,13 @@ app.layout = ddk.App(show_editor=False, theme=theme, children=[
     ])
 ])
 
+
 @callback(
     Output("project_information", "children"),
     Input("sel_project", "value"),
 )
 def update_project_information(sel_project):
-    attributes = {}
+    project_attributes = {}
     text = None
     if sel_project:
         for dataset_ID in dataset_ID_list:
@@ -93,27 +108,33 @@ def update_project_information(sel_project):
                 e.dataset_id = dataset_ID
                 break
         ds = e.to_xarray()
-        attributes = ds.attrs
-        redis_instance.hset("metadata", sel_project, json.dumps(attributes))
-        text = [f"Project: {attributes['project']}\n",
-                f"Platform: {attributes['platform']}\n",
-                f"Start Date: {datetime.fromisoformat(attributes['time_coverage_start'][:-1]).astimezone(timezone.utc)}\n",
-                f"End Date: {datetime.fromisoformat(attributes['time_coverage_end'][:-1]).astimezone(timezone.utc)}\n",
-                f"{attributes['summary']}"]
+        project_attributes = ds.attrs
+        redis_instance.hset("project_metadata", sel_project, json.dumps(project_attributes))
+        text = [f"Project: {project_attributes['project']}\n",
+                f"Platform: {project_attributes['platform']}\n",
+                f"Start Date: {datetime.fromisoformat(project_attributes['time_coverage_start'][:-1]).astimezone(timezone.utc)}\n",
+                f"End Date: {datetime.fromisoformat(project_attributes['time_coverage_end'][:-1]).astimezone(timezone.utc)}\n",
+                f"{project_attributes['summary']}"]
     return text
 
 
 @callback(
     Output("dataset_options", "options"),
+    #Output("data_set_display", "value"),
     Input("sel_project", "value"),
 )
 def update_dataset_options(sel_project):
     IDs = []
     for dataset_ID in dataset_ID_list:
+        ID_options_dict ={}
         if sel_project:
             if sel_project in dataset_ID:
-                IDs.append(dataset_ID)
-    return IDs
+                #IDs.append(dataset_ID)
+                ID_display = dataset_ID.split('_', 3)[3]
+                ID_options_dict['label'] = ID_display
+                ID_options_dict['value'] = dataset_ID
+                IDs.append(ID_options_dict)
+    return IDs#, ID_display
 
 
 @callback(
@@ -127,27 +148,76 @@ def update_variable_options(sel_dataset):
         # ds = e.to_xarray()
         # for var in ds.data_vars:
         #     variables.append(var)
-        if redis_instance.hget("data", sel_dataset):
+        if redis_instance.hget("data", sel_dataset) and redis_instance.hget("dataset_metadata", sel_dataset):
             ds = pd.DataFrame(json.loads(redis_instance.hget("data", sel_dataset)))
+            meta = json.loads(redis_instance.hget("dataset_metadata", sel_dataset))
         else:
             ds = e.to_pandas()
+            meta = e.to_xarray()
+            dataset_attributes = meta.attrs
             redis_instance.hset("data", sel_dataset, ds.to_json())
+            redis_instance.hset("dataset_metadata", sel_dataset, json.dumps(dataset_attributes))
         for var in ds:
             variables.append(var)
     return variables
 
 
 @callback(
-    Output("timeseries", "figure"),
+    #Output("1D_timeseries", "figure"),
+    Output("graph", "figure"),
     Input("variables", "value"),
-    Input("dataset_options", "value")
+    Input("dataset_options", "value"),
+    Input("sel_project", "value")
 )
-def plot_timeseries(data_var, sel_dataset):
+def plot_1D_timeseries(data_var, sel_dataset, sel_project):
     if data_var:
-        ds = pd.DataFrame(json.loads(redis_instance.hget("data", sel_dataset)))
-        fig = px.scatter(ds, x='time (UTC)', y=data_var)
-        fig.update_layout()
-        return fig
+        metadata = json.loads(redis_instance.hget("dataset_metadata", sel_dataset))
+        if len(metadata['dimensions'].split()) == 1:
+            ds = pd.DataFrame(json.loads(redis_instance.hget("data", sel_dataset)))
+            fig = px.scatter(ds, x='time (UTC)', y=data_var)
+            fig.update_layout(margin=dict(l=60, r=60, t=60, b=60))
+            return fig
+        else:
+            return None
+
+@callback(
+    #Output("2D_timeseries", "figure"),
+    Output("graph", "figure", allow_duplicate=True),
+    Input("variables", "value"),
+    Input("dataset_options", "value"),
+    Input("sel_project", "value"),
+    prevent_initial_call=True
+)
+def plot2D_timeseries(data_var, sel_dataset, sel_project):
+    if data_var:
+        print(data_var)
+        metadata = json.loads(redis_instance.hget("dataset_metadata", sel_dataset))
+        dims = metadata['dimensions'].split()
+        if len(metadata['dimensions'].split()) == 2:
+            #ds = e.to_xarray
+            ds = pd.DataFrame(json.loads(redis_instance.hget("data", sel_dataset)))
+            #fig = px.imshow(ds)
+            max_val = ds[data_var].max()
+            fig = go.Figure(data = go.Heatmap(z = ds[data_var], x = ds.iloc[:,0], y = ds.iloc[:,1]#,
+            # colorscale= [
+            # [0, 'rgb(255, 0, 255)'],        #0
+            # [1./10000, 'rgb(0, 0, 255)'], #10
+            # [1./1000, 'rgb(0, 255, 255)'],  #100
+            # [1./100, 'rgb(0, 255, 0)'],   #1000
+            # [1./10, 'rgb(255, 255, 0)'],       #10000
+            # [1., 'rgb(255, 0, 0)'],             #100000
+            # ],
+            # colorbar = dict(
+            #     tick0 = 0,
+            #     tickmode = 'array',
+            #     tickvals = [0, max_val/1000, max_val/10, max_val]
+             ))#)
+            fig.update_yaxes(type='log')
+            fig.update_layout(xaxis_title = "Time", yaxis_title = "Diameter (micrometers)", title = data_var, margin={'t': 50})
+            fig.show()
+            return fig
+        else:
+            return None
 
 
 @callback(
@@ -161,7 +231,7 @@ def plot_trajectory(sel_dataset):
         else:
             ds = e.to_pandas()
             redis_instance.hset("data", sel_dataset, ds.to_json())
-        ds = pd.DataFrame(json.loads(redis_instance.hget("data", sel_dataset)))
+        #ds = pd.DataFrame(json.loads(redis_instance.hget("data", sel_dataset)))
         fig = px.scatter_map(ds, lat='latitude (degrees_north)', lon='longitude (degrees_east)', zoom=6, 
                         center={"lat": ds['latitude (degrees_north)'].mean(), "lon": ds['longitude (degrees_east)'].mean()})
         fig.update_geos(showcoastlines=True, coastlinecolor="RebeccaPurple",
@@ -172,5 +242,7 @@ def plot_trajectory(sel_dataset):
         fig.update_layout()
         return fig
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
